80bafe111ca6375fa6cb9e98b45eaffd8dfc27a3
Marc Beillevaire <marc.beillevaire@gmail.com>
2020-06-23 19:09:56 +0200

    Adding ternary tree class + parentheses-tree builder

---
+++ internal/regexparser/ternary_tree.go
@@ -0,0 +1,25 @@
+package regexparser
+
+
+type TernaryNode struct {
+	Value string
+	Start int
+	End int
+	LeftChild *TernaryNode
+	MiddleChild *TernaryNode
+	RightChild *TernaryNode
+}
+
+func NewTernaryNode(value string, start int, end int) TernaryNode {
+	return TernaryNode{value, start, end, nil, nil, nil}
+}
+
+func NewRootTernaryNode(value string) TernaryNode {
+	return TernaryNode{value, 0, len(value) - 1, nil, nil, nil}
+}
+
+func NewLeafTernaryNode(value string, pos int) TernaryNode {
+	return TernaryNode{value, pos, pos + 1, nil, nil, nil}
+}
+
+
